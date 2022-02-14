1b468f3a762ca4e2f3c609533d97fdff1941a143
Lolita Nazarov <ln3141592653@gmail.com>
2022-02-13 19:34:46 -0500

    restructured

---
+++ src/components/VenueLocationIcon.js
@@ -0,0 +1,16 @@
+import L from 'leaflet';
+
+/*
+creates an object for the custom marker icon for the leaflet map
+*/
+
+export const VenueLocationIcon = L.icon({
+  iconUrl: require('../assets/venue_location_icon.svg'),
+  iconRetinaUrl: require('../assets/venue_location_icon.svg'),
+  iconAnchor: null,
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: [35, 35],
+  className: 'leaflet-venue-icon'
+});
