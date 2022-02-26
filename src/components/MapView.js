9aaf7ddd9bedd89b8be0bcefbde2d4ce632f65fe
christina511 <christina511ny@gmail.com>
2022-02-25 22:26:57 -0500

    toggle choose border lines for district, precinct, and county working.
    next step is to connect this with the geojson files for it to show

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,4 +1,4 @@
-import React, {useState } from 'react';
+import { useState } from 'react';
 import { MapContainer, TileLayer, GeoJSON, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -7,12 +7,10 @@ import tennessee from "../assets/tennessee_congressional_districts.json";
 import southcarolina from "../assets/southcarolina_congressional.json";
 import tennesseeOutline from "../assets/tennessee.json";
 import southcarolinaOutline from "../assets/southcarolina.json";
-import './style/Legend.css';
 // components
 import Navigation from './Navigation';
 import Sidebar from './Sidebar';
 import BottomTab from './BottomTab';
-import Legend from './Legend';
 
 /*
 currentLocation contains fallback coordinates of the center of the United States
@@ -26,7 +24,10 @@ const MapView = (props) => {
     zoom: 5,
     name: 'USA',
     layer: null, //this is used to remove geojson layer -> might need to delete later on
-    view: 'election'
+    view: 'election',
+    districtbord: true,
+    precinctbord: false,
+    countybord: false
   });
 
   //sidebar
@@ -56,19 +57,11 @@ const MapView = (props) => {
   const openDrawer = () => setIsVisible(true);
   const closeDrawer = () => setIsVisible(false);
 
-  // the modal stuff (District comparison)
-  const [isOpenModal, setIsOpenModal] = React.useState(false);
-  const showModal = () => {
-    setIsOpenModal(true);
-  };
-  const hideModal = () => {
-    setIsOpenModal(false);
-  };
-
   //district hovering functions
   const highlight = (feature, layer) => {
     layer.on({
       mouseover: highlightFeature
+      // ,
       // mouseout: resetHighlight
     });
   }
@@ -77,13 +70,9 @@ const MapView = (props) => {
     var layer = e.target;
     const pop = layer.feature.properties.POPULATION;
     const district = layer.feature.properties.DISTRICT;
-    const incumbent = layer.feature.properties.INCUMBENT;
-    const lean = layer.feature.properties.LEAN;
     setOnselect({
       population: pop,
       district: district,
-      incumbent: incumbent,
-      lean: lean
     });
     // layer.setStyle({
     //   weight: 1,
@@ -111,7 +100,7 @@ const MapView = (props) => {
   
   function zoomState(state, layer) {
     setOnselect({}); //resets the info box if user clicks on a new state
-    // if(currentLocation.view !== 'population') document.getElementsByClassName("legend")[0].classList.add('hidden');
+    if(currentLocation.view !== 'population') document.getElementsByClassName("legend")[0].classList.add('hidden');
 
     var polygon = new L.Polygon(state.geometry.coordinates);
     var bounds = polygon.getBounds();
@@ -128,14 +117,17 @@ const MapView = (props) => {
       zoom: 7, 
       name: state.properties.name, 
       layer: layer, 
-      view: currentLocation.view
+      view: currentLocation.view,
+      districtbord: currentLocation.districtbord,
+      precinctbord: currentLocation.precinctbord,
+      countybord: currentLocation.countybord
     });
     handleShow();
     // state.data = state.properties.name.toLowerCase();
 
     //changes css to show boxes
     document.getElementsByClassName("info-box")[0].classList.remove('hidden');
-    document.getElementsByClassName("legend")[0].classList.remove('hidden');
+    // document.getElementsByClassName("legend")[0].classList.remove('hidden');
   }
 
   function MyComponent() {
@@ -155,26 +147,32 @@ const MapView = (props) => {
   // }
 
   function changeView(v){
-    // if(v === 'population') document.getElementsByClassName("legend")[0].classList.remove('hidden');
+    if(v === 'population') document.getElementsByClassName("legend")[0].classList.remove('hidden');
     setLocation({
       center: currentLocation.center, 
       zoom: currentLocation.zoom, 
       name: currentLocation.name, 
       layer: currentLocation.layer,
-      view: v});
-    console.log("change view current view: " + currentLocation.view)
+      view: v,
+      districtbord: currentLocation.districtbord,
+      precinctbord: currentLocation.precinctbord,
+      countybord: currentLocation.countybord
+    });
+    console.log("change view current view: "+ currentLocation.view)
   }
 
   function setStyle(feature) {
+    console.log("district" + currentLocation.districtbord);
+    console.log("precinct" + currentLocation.precinctbord);
+    console.log("county" + currentLocation.countybord);
     let style = currentLocation.view;
         return{
           //fill property shows 'red' or 'blue' based on republican/democratic district
           fillColor: feature.fill[style],
-          color: 'black',
-          weight: '1',
+          color: feature.fill[style],
           //color: getColor(feature.properties.TOTAL),
-          // opacity: 0.6,
-          fillOpacity: 0.7
+          opacity:0.8,
+          fillOpacity:0.4
           }
   }
 
@@ -185,6 +183,53 @@ const MapView = (props) => {
     }
   }
 
+  function toggleDistrict(){
+    setLocation({
+      center: currentLocation.center, 
+      zoom: currentLocation.zoom, 
+      name: currentLocation.name, 
+      layer: currentLocation.layer,
+      view: currentLocation.view,
+      districtbord: !currentLocation.districtbord,
+      precinctbord: currentLocation.precinctbord,
+      countybord: currentLocation.countybord
+    })
+    console.log("district" + currentLocation.districtbord);
+    console.log("precinct" + currentLocation.precinctbord);
+    console.log("county" + currentLocation.countybord);
+  }
+
+  function togglePrecinct(){
+    setLocation({
+      center: currentLocation.center, 
+      zoom: currentLocation.zoom, 
+      name: currentLocation.name, 
+      layer: currentLocation.layer,
+      view: currentLocation.view,
+      districtbord: currentLocation.districtbord,
+      precinctbord: !currentLocation.precinctbord,
+      countybord: currentLocation.countybord
+    })
+    console.log("district" + currentLocation.districtbord);
+    console.log("precinct" + currentLocation.precinctbord);
+    console.log("county" + currentLocation.countybord);
+  }
+
+  function toggleCounty(){
+    setLocation({
+      center: currentLocation.center, 
+      zoom: currentLocation.zoom, 
+      name: currentLocation.name, 
+      layer: currentLocation.layer,
+      view: currentLocation.view,
+      districtbord: currentLocation.districtbord,
+      precinctbord: currentLocation.precinctbord,
+      countybord: !currentLocation.countybord
+    })
+    console.log("district" + currentLocation.districtbord);
+    console.log("precinct" + currentLocation.precinctbord);
+    console.log("county" + currentLocation.countybord);
+  }
   /*
   in the render() function the MapContainer() function is returned.
   TileLayer component adds the tiles for the map
@@ -193,7 +238,7 @@ const MapView = (props) => {
 
   return (
     <div id='map'>
-      <Navigation zoomState={zoomState} className='google-maps' changeView={changeView} />
+      <Navigation zoomState={zoomState} className='google-maps' changeView={changeView} toggleDistrict={toggleDistrict} togglePrecinct={togglePrecinct} toggleCounty={toggleCounty}/>
       <MapContainer center={currentLocation.center} zoom={currentLocation.zoom} zoomControl={false} 
         maxBounds={[[19.8283, -130.5795], [54.8283, -58.5795]]} minZoom={5} maxZoom={15}>
         <MyComponent />
@@ -202,7 +247,7 @@ const MapView = (props) => {
         <GeoJSON data={southcarolina} onEachFeature={highlight} style={setStyle}/>
         <GeoJSON data={southcarolinaOutline} onEachFeature={clicked} style={outlineStyle} />
         <GeoJSON data={tennesseeOutline} onEachFeature={clicked} style={outlineStyle} />
-        <Sidebar show={show} handleClose={handleClose} name={currentLocation.name} showModal={showModal} hideModal ={hideModal} isOpenModal={isOpenModal}/>
+        <Sidebar show={show} handleClose={handleClose} name={currentLocation.name} openDrawer={openDrawer} />
 
         <div className="info-box hidden">
           {!onselect.district && (
@@ -214,14 +259,10 @@ const MapView = (props) => {
           {onselect.district && (
             <ul className = "census-info">
               <li><strong>District {onselect.district}</strong></li><br />
-              <li>Total Population: {onselect.population} (needs to be updated)</li>
-              <li>Incumbent: {onselect.incumbent}</li>
-              <li>Partisan Lean: {onselect.lean}</li>
+              <li>Total Population: {onselect.population}</li>
             </ul>
           )}
         </div>
-        <Legend view={currentLocation.view} />
-
       </MapContainer>
 
       <BottomTab isVisible={isVisible} closeDrawer={closeDrawer} />
