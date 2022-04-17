fc1ecc5ebaf398ed36f71a8fcb996b1e4b133b4c
JunLee365 <junholee365@gmail.com>
2022-04-17 17:09:30 -0400

    Found and debugged server side: server can now query data of different states
    Client bug still exists though...

--- server/src/main/java/com/example/server/repository/DemographicRepository.java
+++ server/src/main/java/com/example/server/repository/DemographicRepository.java
@@ -1,21 +1,18 @@
-// EDITING needed
 package com.example.server.repository;
 
-import com.example.server.enumeration.StateCode;
 import com.example.server.model.Demographic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface DemographicRepository extends JpaRepository<Demographic, Integer> {
     // get demographic of state
-    @Query(value = "SELECT * FROM demographic\n" +
-            "WHERE state_id = 5 AND " +
+    @Query(value = "SELECT * FROM demographic " +
+            "WHERE state_id = ?1 AND " +
             "district_plan_id IS null AND " +
             "precinct_id IS null", nativeQuery = true)
-    List<Demographic> findDemographicByStateIdNamedParamsNative(@Param("state_id") StateCode stateId);
+    List<Demographic> findDemographicByStateId(int state_id);
 
     // get demographic of district
 
