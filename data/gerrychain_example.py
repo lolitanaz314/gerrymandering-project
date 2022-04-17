ed4d76ef47617eb8583e1f1ef5d65aaf546b0372
Lolita Nazarov <ln3141592653@gmail.com>
2022-04-17 18:52:20 -0400

    changes

---
+++ data/gerrychain_example.py
@@ -0,0 +1,88 @@
+import geopandas
+import matplotlib.pyplot as plt
+from gerrychain import (GeographicPartition, Partition, Graph, MarkovChain,
+                        proposals, updaters, constraints, accept, Election)
+from gerrychain.proposals import recom
+from functools import partial
+import pandas
+
+#geopandas.options.use_pygeos = False
+
+graph = Graph.from_json("/Users/cherrypi/Desktop/gerrymandering-project/data/sample_data/PA_VTDs_sample.json")
+
+elections = [
+    Election("SEN10", {"Democratic": "SEN10D", "Republican": "SEN10R"}),
+    Election("SEN12", {"Democratic": "USS12D", "Republican": "USS12R"}),
+    Election("SEN16", {"Democratic": "T16SEND", "Republican": "T16SENR"}),
+    Election("PRES12", {"Democratic": "PRES12D", "Republican": "PRES12R"}),
+    Election("PRES16", {"Democratic": "T16PRESD", "Republican": "T16PRESR"})
+]
+
+# Population updater, for computing how close to equality the district
+# populations are. "TOTPOP" is the population column from our shapefile.
+my_updaters = {"population": updaters.Tally("TOTPOP", alias="population")}
+
+# Election updaters, for computing election results using the vote totals
+# from our shapefile.
+election_updaters = {election.name: election for election in elections}
+my_updaters.update(election_updaters)
+
+# We can now instantiate the initial state of our Markov chain, using the 2011 districting plan
+initial_partition = GeographicPartition(graph, assignment="CD_2011", updaters=my_updaters)
+
+# The ReCom proposal needs to know the ideal population for the districts so that
+# we can improve speed by bailing early on unbalanced partitions.
+
+ideal_population = sum(initial_partition["population"].values()) / len(initial_partition)
+
+# We use functools.partial to bind the extra parameters (pop_col, pop_target, epsilon, node_repeats)
+# of the recom proposal.
+proposal = partial(recom,
+                   pop_col="TOTPOP",
+                   pop_target=ideal_population,
+                   epsilon=0.02,
+                   node_repeats=2
+                  )
+
+compactness_bound = constraints.UpperBound(
+    lambda p: len(p["cut_edges"]),
+    2*len(initial_partition["cut_edges"])
+)
+
+pop_constraint = constraints.within_percent_of_ideal_population(initial_partition, 0.02)
+
+# Configuring the markov chain 
+
+chain = MarkovChain(
+    proposal=proposal,
+    constraints=[
+        pop_constraint,
+        compactness_bound
+    ],
+    accept=accept.always_accept,
+    initial_state=initial_partition,
+    total_steps=1000
+)
+
+# This will take about 10 minutes.
+data = pandas.DataFrame(
+    sorted(partition["SEN12"].percents("Democratic"))
+    for partition in chain
+)
+
+fig, ax = plt.subplots(figsize=(8, 6))
+
+# Draw 50% line
+ax.axhline(0.5, color="#cccccc")
+# Draw boxplot
+data.boxplot(ax=ax, positions=range(len(data.columns)))
+# Draw initial plan's Democratic vote %s (.iloc[0] gives the first row)
+plt.plot(data.iloc[0], "ro")
+# Annotate
+ax.set_title("Comparing the 2011 plan to an ensemble")
+ax.set_ylabel("Democratic vote % (Senate 2012)")
+ax.set_xlabel("Sorted districts")
+ax.set_ylim(0, 1)
+ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
+
+plt.show()
---
+++ data/test_gerrychain.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+from gerrychain import (GeographicPartition, Partition, Graph, MarkovChain,
+                        proposals, updaters, constraints, accept, Election)
+from gerrychain.proposals import recom
+from functools import partial
+import pandas
+import sys
