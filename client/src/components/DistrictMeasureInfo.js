6746ffb79de6233127c4ae200ba68053dcbf2375
Jenny Fu <jenny.fu@stonybrook.edu>
2022-04-20 21:59:14 -0400

    moved radar data to dmi

--- client/src/components/DistrictMeasureInfo.js
+++ client/src/components/DistrictMeasureInfo.js
@@ -1,14 +1,41 @@
-import statemeasures from '../assets/img/state_measures.png'
 import SeatVoteCurve from './SeatVoteCurve';
-
+import RadarCharting from './RadarChart';
 import Table from 'react-bootstrap/Table';
 import DistrictPlanMeasuresTable from './DistrictPlanMeasuresTable';
-import RadarCharting from './RadarChart';
 
-const imageComponentSidebar = {
-    width: 550,
-    height: 200
-}
+// fake data
+const data = [
+    {
+        "measure": "Majority-Minority Districts",
+        "A": 120,
+        "B": 110,
+        "fullMark": 150
+    },
+    {
+        "measure": "Efficiency Gap",
+        "A": 98,
+        "B": 130,
+        "fullMark": 150
+    },
+    {
+        "measure": "Competitive Districts",
+        "A": 86,
+        "B": 130,
+        "fullMark": 150
+    },
+    {
+        "measure": "Projected Political Fairness",
+        "A": 99,
+        "B": 100,
+        "fullMark": 150
+    },
+    {
+        "measure": "Compactness",
+        "A": 85,
+        "B": 90,
+        "fullMark": 150
+    },
+];
 
 const curveStyle = { //style for selected dp
     height: '330px',
@@ -20,7 +47,7 @@ const comparingStyle = { //style for compare view (seat plot)
     width: '300px'
 }
 
-const tableStyle = { //style for compare view (img)
+const tableStyle = { //style for compare view (measure table)
     // width: 300,
     height: 200,
 }
@@ -29,15 +56,15 @@ const DistrictMeasureInfo = (props) => {
 
     let basic = <>
         <h5 id="more-measures">Measures</h5>
-        <div> <DistrictPlanMeasuresTable/> </div>
+        <div> <DistrictPlanMeasuresTable /> </div>
         <br></br>
 
         {/* only here for now. should only show up when comparing 2 dp? */}
         <h5 id="radar-chart">Radar Chart</h5>
-        <div> <RadarCharting/> </div>
-        
+        <div> <RadarCharting data={data} /> </div>
+
         <h5 id="seat-vote"> Measure of Fairness - Seats to Vote</h5>
-        <div style={curveStyle}> <SeatVoteCurve/> </div>
+        <div style={curveStyle}> <SeatVoteCurve /> </div>
         <br></br>
     </>;
 
@@ -46,8 +73,8 @@ const DistrictMeasureInfo = (props) => {
         basic = <Table>
             <thead>
                 <tr>
-                    <th style={{float:'left'}}>District Plan {props.currentDp}</th>
-                    <th style={{float:'right'}}>District Plan {props.pinned}</th>
+                    <th style={{ float: 'left' }}>District Plan {props.currentDp}</th>
+                    <th style={{ float: 'right' }}>District Plan {props.pinned}</th>
                 </tr>
             </thead>
 
@@ -55,22 +82,22 @@ const DistrictMeasureInfo = (props) => {
                 <Table className='column'>
                     <td>
                         <h5 id="more-measures">Measures</h5>
-                        <div style={tableStyle}> <DistrictPlanMeasuresTable/> </div>
+                        <div style={tableStyle}> <DistrictPlanMeasuresTable /> </div>
                         <br></br>
 
                         <h5 id="seat-vote">Measure of Fairness - Seats to Vote</h5>
-                        <div style={comparingStyle}> <SeatVoteCurve/> </div>
+                        <div style={comparingStyle}> <SeatVoteCurve /> </div>
                     </td>
                 </Table>
 
                 <Table className='column'>
                     <td>
                         <h5 id="more-measures">Measures</h5>
-                        <div style={tableStyle}> <DistrictPlanMeasuresTable/> </div>
+                        <div style={tableStyle}> <DistrictPlanMeasuresTable /> </div>
                         <br></br>
 
                         <h5 id="seat-vote">Measure of Fairness - Seats to Vote</h5>
-                        <div style={comparingStyle}> <SeatVoteCurve/> </div>
+                        <div style={comparingStyle}> <SeatVoteCurve /> </div>
                     </td>
                 </Table>
             </tbody>
--- client/src/components/RadarChart.js
+++ client/src/components/RadarChart.js
@@ -1,44 +1,9 @@
 import React from 'react';
-import { Radar, RadarChart, PolarGrid, Legend,
-    PolarAngleAxis, PolarRadiusAxis } from 'recharts';
+import { Radar, RadarChart, PolarGrid, Legend, PolarAngleAxis, PolarRadiusAxis } from 'recharts';
   
-const RadarCharting = () => {
-    // Sample data
-    const data = [
-        {
-            "measure": "Majority-Minority Districts",
-            "A": 120,
-            "B": 110,
-            "fullMark": 150
-          },
-          {
-            "measure": "Efficiency Gap",
-            "A": 98,
-            "B": 130,
-            "fullMark": 150
-          },
-          {
-            "measure": "Competitive Districts",
-            "A": 86,
-            "B": 130,
-            "fullMark": 150
-          },
-          {
-            "measure": "Projected Political Fairness",
-            "A": 99,
-            "B": 100,
-            "fullMark": 150
-          },
-          {
-            "measure": "Compactness",
-            "A": 85,
-            "B": 90,
-            "fullMark": 150
-          },
-    ];
-  
+const RadarCharting = (props) => {
     return (
-        <RadarChart outerRadius={90} width={730} height={250} data={data}>
+        <RadarChart outerRadius={90} width={730} height={250} data={props.data}>
             <PolarGrid />
             <PolarAngleAxis dataKey="measure" />
             <PolarRadiusAxis angle={30} domain={[0, 150]} />
