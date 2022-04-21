4d78cd1f421e107d813ac06cb296e4a6d65dc325
christina511 <christina511ny@gmail.com>
2022-04-20 21:54:51 -0400

    a little nicer radar chart LOL

--- client/src/components/RadarChart.js
+++ client/src/components/RadarChart.js
@@ -1,29 +1,50 @@
 import React from 'react';
-import { Radar, RadarChart, PolarGrid, 
+import { Radar, RadarChart, PolarGrid, Legend,
     PolarAngleAxis, PolarRadiusAxis } from 'recharts';
   
 const RadarCharting = () => {
     // Sample data
     const data = [
-        { name: 'A', x: 21 },
-        { name: 'B', x: 22 },
-        { name: 'C', x: -32 },
-        { name: 'D', x: -14 },
-        { name: 'E', x: -51 },
-        { name: 'F', x: 16 },
-        { name: 'G', x: 7 },
-        { name: 'H', x: -8 },
-        { name: 'I', x: 9 },
+        {
+            "measure": "Majority-Minority Districts",
+            "A": 120,
+            "B": 110,
+            "fullMark": 150
+          },
+          {
+            "measure": "Efficiency Gap",
+            "A": 98,
+            "B": 130,
+            "fullMark": 150
+          },
+          {
+            "measure": "Competitive Districts",
+            "A": 86,
+            "B": 130,
+            "fullMark": 150
+          },
+          {
+            "measure": "Projected Political Fairness",
+            "A": 99,
+            "B": 100,
+            "fullMark": 150
+          },
+          {
+            "measure": "Compactness",
+            "A": 85,
+            "B": 90,
+            "fullMark": 150
+          },
     ];
   
     return (
-        <RadarChart height={500} width={500} 
-            outerRadius="80%" data={data}>
+        <RadarChart outerRadius={90} width={730} height={250} data={data}>
             <PolarGrid />
-            <PolarAngleAxis dataKey="name" />
-            <PolarRadiusAxis />
-            <Radar dataKey="x" stroke="green" 
-                fill="green" fillOpacity={0.5} />
+            <PolarAngleAxis dataKey="measure" />
+            <PolarRadiusAxis angle={30} domain={[0, 150]} />
+            <Radar name="District Plan A" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+            <Radar name="District Plan B" dataKey="B" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6} />
+            <Legend />
         </RadarChart>
     );
 }
