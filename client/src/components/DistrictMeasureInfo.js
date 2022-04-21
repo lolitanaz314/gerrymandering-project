925b69fd154c0723d4055d38fa9952e07212b733
Jenny Fu <jenny.fu@stonybrook.edu>
2022-04-20 22:15:01 -0400

    comapring for radar plot

--- client/src/components/DistrictMeasureInfo.js
+++ client/src/components/DistrictMeasureInfo.js
@@ -8,36 +8,36 @@ const data = [
     {
         "measure": "Majority-Minority Districts",
         "A": 120,
-        "B": 110,
+        "B": 100,
         "fullMark": 150
     },
     {
         "measure": "Efficiency Gap",
         "A": 98,
-        "B": 130,
+        "B": 120,
         "fullMark": 150
     },
     {
         "measure": "Competitive Districts",
         "A": 86,
-        "B": 130,
+        "B": 90,
         "fullMark": 150
     },
     {
         "measure": "Projected Political Fairness",
         "A": 99,
-        "B": 100,
+        "B": 50,
         "fullMark": 150
     },
     {
         "measure": "Compactness",
         "A": 85,
-        "B": 90,
+        "B": 70,
         "fullMark": 150
     },
 ];
 
-const curveStyle = { //style for selected dp
+const curveStyle = { //style for graphs
     height: '330px',
     width: '650px'
 }
@@ -49,7 +49,7 @@ const comparingStyle = { //style for compare view (seat plot)
 
 const tableStyle = { //style for compare view (measure table)
     // width: 300,
-    height: 200,
+    height: 200
 }
 
 const DistrictMeasureInfo = (props) => {
@@ -59,49 +59,55 @@ const DistrictMeasureInfo = (props) => {
         <div> <DistrictPlanMeasuresTable /> </div>
         <br></br>
 
-        {/* only here for now. should only show up when comparing 2 dp? */}
-        <h5 id="radar-chart">Radar Chart</h5>
-        <div> <RadarCharting data={data} /> </div>
-
         <h5 id="seat-vote"> Measure of Fairness - Seats to Vote</h5>
         <div style={curveStyle}> <SeatVoteCurve /> </div>
+        <br></br>
+
+        <h5 id="radar-chart">Radar Chart</h5>
+        <div style={curveStyle}> <RadarCharting data={data} currentDp={props.currentDp} comparing={props.compare} /> </div>
         <br></br>
     </>;
 
     //IF USER CLICKS COMPARE BUTTON, CHANGE THE VIEW
     if (props.compare) {
-        basic = <Table>
-            <thead>
-                <tr>
-                    <th style={{ float: 'left' }}>District Plan {props.currentDp}</th>
-                    <th style={{ float: 'right' }}>District Plan {props.pinned}</th>
-                </tr>
-            </thead>
+        basic = <>
+            <Table>
+                <thead>
+                    <tr>
+                        <th style={{ float: 'left' }}>District Plan {props.currentDp}</th>
+                        <th style={{ float: 'right' }}>District Plan {props.pinned}</th>
+                    </tr>
+                </thead>
 
-            <tbody>
-                <Table className='column'>
-                    <td>
-                        <h5 id="more-measures">Measures</h5>
-                        <div style={tableStyle}> <DistrictPlanMeasuresTable /> </div>
-                        <br></br>
+                <tbody>
+                    <Table className='column'>
+                        <td>
+                            <h5 id="more-measures">Measures</h5>
+                            <div style={tableStyle}> <DistrictPlanMeasuresTable /> </div>
+                            <br></br>
 
-                        <h5 id="seat-vote">Measure of Fairness - Seats to Vote</h5>
-                        <div style={comparingStyle}> <SeatVoteCurve /> </div>
-                    </td>
-                </Table>
+                            <h5 id="seat-vote">Measure of Fairness - Seats to Vote</h5>
+                            <div style={comparingStyle}> <SeatVoteCurve /> </div>
+                        </td>
+                    </Table>
 
-                <Table className='column'>
-                    <td>
-                        <h5 id="more-measures">Measures</h5>
-                        <div style={tableStyle}> <DistrictPlanMeasuresTable /> </div>
-                        <br></br>
+                    <Table className='column'>
+                        <td>
+                            <h5 id="more-measures">Measures</h5>
+                            <div style={tableStyle}> <DistrictPlanMeasuresTable /> </div>
+                            <br></br>
 
-                        <h5 id="seat-vote">Measure of Fairness - Seats to Vote</h5>
-                        <div style={comparingStyle}> <SeatVoteCurve /> </div>
-                    </td>
-                </Table>
-            </tbody>
-        </Table>
+                            <h5 id="seat-vote">Measure of Fairness - Seats to Vote</h5>
+                            <div style={comparingStyle}> <SeatVoteCurve /> </div>
+                        </td>
+                    </Table>
+                </tbody>
+            </Table>
+            <h5 id="radar-chart">Radar Chart</h5>
+            <div style={curveStyle}>
+                <RadarCharting data={data} currentDp={props.currentDp} pinnedDp={props.pinned} comparing={props.compare} />
+            </div>
+        </>
     }
 
     return (basic);
--- client/src/components/RadarChart.js
+++ client/src/components/RadarChart.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Radar, RadarChart, PolarGrid, Legend, PolarAngleAxis, PolarRadiusAxis } from 'recharts';
+import { Radar, RadarChart, PolarGrid, Legend, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
   
 const RadarCharting = (props) => {
+
+    let radar = <Radar name={"District Plan #" + props.currentDp} dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+    if(props.comparing){
+        radar = <>
+            <Radar name={"District Plan #" + props.currentDp} dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+            <Radar name={"District Plan #" + props.pinnedDp} dataKey="B" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6} />
+        </>
+    }
+
     return (
+    <ResponsiveContainer width={'100%'} height={'100%'}>
         <RadarChart outerRadius={90} width={730} height={250} data={props.data}>
             <PolarGrid />
             <PolarAngleAxis dataKey="measure" />
             <PolarRadiusAxis angle={30} domain={[0, 150]} />
-            <Radar name="District Plan A" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-            <Radar name="District Plan B" dataKey="B" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6} />
+            {radar}
             <Legend />
         </RadarChart>
+    </ResponsiveContainer>
     );
 }
   
