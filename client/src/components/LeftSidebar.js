d90c4d24bc1f17508aec3b5b6dcb1c3b98d6697b
christina511 <christina511ny@gmail.com>
2022-04-28 19:56:38 -0400

    data for other team states + statuses

--- client/src/components/LeftSidebar.js
+++ client/src/components/LeftSidebar.js
@@ -1,6 +1,104 @@
 import { Offcanvas } from 'react-bootstrap';
 import React from 'react';
 
+const teamStates = [ //status: 0 for proposed, 1 for approved
+    {
+    name: 'Alabama',
+    status: 1, 
+    teams: ['Aces']
+},
+{
+    name: 'Arizona',
+    status: 1, 
+    teams: ['Aces']
+},
+{
+    name: 'Arkansas',
+    status: 1, 
+    teams: ['Aces']
+},
+{
+    name: 'California',
+    status: 1, 
+    teams: ['Sky']
+},
+{
+    name: 'Colorado',
+    status: 1, 
+    teams: ['Lynx']
+},
+{
+    name: 'Florida',
+    status: 1, 
+    teams: ['Dream']
+},
+{
+    name: 'Georgia',
+    status: 1, 
+    teams: ['Drean']
+},
+{
+    name: 'Illinois',
+    status: 1, 
+    teams: ['Sparks']
+},
+{
+    name: 'Louisiana',
+    status: 1, 
+    teams: ['Sparks']
+},
+{
+    name: 'Maryland',
+    status: 1, 
+    teams: ['Wings']
+},
+{
+    name: 'Michigan',
+    status: 1, 
+    teams: ['Wings']
+},
+{
+    name: 'Mississippi',
+    status: 1, 
+    teams: ['Dream', 'Mystics']
+},
+{
+    name: 'Nevada',
+    status: 1, 
+    teams: ['Sparks', 'Fever']
+},
+{
+    name: 'New York',
+    status: 0, 
+    teams: ['Sky']
+},
+{
+    name: 'North Carolina',
+    status: 1, 
+    teams: ['Mystics']
+},
+{
+    name: 'South Carolina',
+    status: 1, 
+    teams: ['Fever', 'Lynx']
+},
+{
+    name: 'Tennessee',
+    status: 1, 
+    teams: ['Lynx', 'Mystics']
+},
+{
+    name: 'Texas',
+    status: 1, 
+    teams: ['Fever']
+},
+{
+    name: 'Virginia',
+    status: 1, 
+    teams: ['Wings']
+}
+];
+
 const sidebarStyle = {
     margin: '56px 0px 0px 0px',
     width: '20%',
