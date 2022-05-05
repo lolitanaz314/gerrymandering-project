199dcbef7ce34aadb4c76f2a89b419657298c579
Lolita Nazarov <ln3141592653@gmail.com>
2022-05-05 11:29:06 -0400

    final workable district plan generation code

--- data/pre-processing/adding_demographic_data_to_precincts/add_demographic_data_to_precinct_co.py
+++ data/pre-processing/adding_demographic_data_to_precincts/add_demographic_data_to_precinct_co.py
@@ -4,18 +4,43 @@ import maup
 import json
 import numpy as np
 
+from shapely.validation import make_valid
+from shapely.geometry.base import geom_factory
+from shapely.geos import lgeos
 import warnings; warnings.filterwarnings('ignore', 'GeoSeries.isna', UserWarning)
 
-precincts_co=geopandas.read_file("/Users/cherrypi/Desktop/gerrymandering-project/data/co_data/precinct/co_precinct_and_election_shp")
-precincts_co.to_crs(inplace=True, crs="EPSG:4269")
-display(precincts_co)
+# Steps:
 
-block_co=geopandas.read_file("/Users/cherrypi/Desktop/gerrymandering-project/data/co_data/precinct/co_cvap_2020_block_shp")
-block_co.to_crs(inplace=True, crs="EPSG:4269")
-display(block_co)
-print(block_co.columns)
+# 1) assign blocks to precincts (in order to add demographic data)
+# 2) assign precincts to a given district (for figuring out the population distribution criteria for MGGG)
+# 3) precinct geopandas now has districtID and demographic data
+# 4) turn the geopandas dataframe into a geojson for gerrychain (MGGG) to turn into a graph
+
+precincts_co=gpd.read_file("/Users/cherrypi/Desktop/gerrymandering-project/data/co_data/precinct/co_precinct_and_election_20_retouched")
+precincts_co.to_crs(inplace=True, crs="EPSG:4326")
+#display(precincts_co)
+
+block_co=gpd.read_file("/Users/cherrypi/Desktop/gerrymandering-project/data/co_data/precinct/co_cvap_2020_block_shp")
+block_co.to_crs(inplace=True, crs="EPSG:4326")
+#display(block_co)
 
 assignment_co=maup.assign(block_co, precincts_co) # this returns a Pandas Series that assigns blocks to precincts
+
+
+# making the geometries valid
+
+precincts_co['geometry']=precincts_co['geometry'].buffer(0)
+
+new_geom_series=[]
+for geom in precincts_co['geometry']:
+    
+    if geom.is_valid:
+        new_geom_series.append(geom)
+    else:
+        new_geom_series.append(geom_factory(lgeos.GEOSMakeValid(geom._geom)))
+
+precincts_co['geometry'] = new_geom_series
+
 
 variables_co = ["CVAP_TOT20", "CVAP_ASN20", "CVAP_BLK20", "CVAP_NHP20", "CVAP_WHT20", "CVAP_HSP20", "CVAP_2OM20"]
 precincts_co[variables_co] = block_co[variables_co].groupby(assignment_co).sum()
@@ -39,9 +64,15 @@ final_precincts_co["totalPres"]= final_precincts_co["democraticPres"]+final_prec
 final_precincts_co["democraticPres"] = final_precincts_co["democraticPres"] / final_precincts_co["totalPres"]
 final_precincts_co["republicanPres"] = final_precincts_co["republicanPres"] / final_precincts_co["totalPres"]
 
+final_precincts_co["democraticPres"]=final_precincts_co["democraticPres"].fillna(0) # replace naans with 0's just in case
+final_precincts_co["republicanPres"]=final_precincts_co["republicanPres"].fillna(0)
+
 final_precincts_co["totalSen"]=final_precincts_co["democraticSen"]+final_precincts_co["republicanSen"]
 final_precincts_co["democraticSen"] = final_precincts_co["democraticSen"] / final_precincts_co["totalSen"]
 final_precincts_co["republicanSen"] = final_precincts_co["republicanSen"] / final_precincts_co["totalSen"]
+
+final_precincts_co["democraticSen"]=final_precincts_co["democraticSen"].fillna(0) # replace naans with 0's just in case
+final_precincts_co["republicanSen"]=final_precincts_co["republicanSen"].fillna(0)
 
 final_precincts_co.drop(['totalSen', 'totalPres'], axis=1, inplace=True)
 
@@ -56,59 +87,12 @@ final_precincts_co['two_or_more']=final_precincts_co['two_or_more'].astype(int)
 
 final_precincts_co['stateId']=final_precincts_co['stateId'].replace("08", "Co")
 
-
-# Assigning to a district for an initial partition 
-
-district_co=geopandas.read_file("/Users/cherrypi/Desktop/gerrymandering-project/data/co_data/district/CO_House_Of_Rep_Districts_Prelim_PROPOSED_shp")
+district_co=gpd.read_file("/Users/cherrypi/Desktop/gerrymandering-project/data/co_data/district/CO_House_Of_Rep_Districts_Prelim_PROPOSED_shp")
 
 final_precincts_co.to_crs(district_co.crs, inplace=True)
 assignment_prec_to_district_co = maup.assign(final_precincts_co, district_co)
 final_precincts_co["districtId"] = assignment_prec_to_district_co
 
-geojson = {
-        "type": "FeatureCollection",
-        "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::4269" } },
-        "features": []
-}
+# and then final_precincts_co would be the DataFrame being turned into a graph and getting inserted into MGGG
 
-for i in range(len(final_precincts_co)):
-    #print(i)
-    insideDict={}  # this is ONE precinct being added!!!
-    onedict=final_precincts_co.iloc[i].to_dict()
-    geom=onedict.pop('geometry')
-
-    insideDict["type"] = "Feature"
-    insideDict["properties"] = onedict
-
-    insideDict["geometry"]={}
-    insideDict["geometry"]['type'] = geom.type
-    
-    if (insideDict["geometry"]['type'] == 'MultiPolygon'):
-        insideDict["geometry"]['coordinates'] = [list(x.exterior.coords) for x in geom.geoms] # multipolygons
-    elif (insideDict["geometry"]['type'] == 'Polygon'):
-        insideDict["geometry"]['coordinates'] = [[thing[0], thing[1]] for thing in geom.exterior.coords]
-
-    geojson["features"].append(insideDict)
-
-with open("/Users/cherrypi/cse416/processed_preseawulf_precincts_path/final_precinct_co.json", 'w') as outfile:
-    json.dump(geojson, outfile, indent=1)
-
-'''
-print(final_precincts_co.columns)  # just to check
-
-f = open("/Users/cherrypi/cse416/processed_preseawulf_precincts_path/final_precinct_co.json")
- 
-# returns JSON object as
-# a dictionary
-data = json.load(f)
-# get geometries and set it manually
-precincts=data["features"]
-geo_list=[]
-for i, p in enumerate(precincts):
-    geo=p["geometry"]
-    geo_list.append(geo)
-
-geo_series_list = pd.Series(geo_list)
-
-'''
 
--- data/seawulf_mggg.py
+++ data/seawulf_mggg.py
@@ -1,15 +1,10 @@
-import geopandas
-import pandas as pd
 from shapely.ops import unary_union
 from gerrychain.accept import always_accept
 import numpy as np
-from gerrychain import (Partition, Graph, MarkovChain, updaters)
+from gerrychain import (Partition, Graph, MarkovChain, updaters, constraints)
 from gerrychain.proposals import recom
 from functools import partial
 from gerrychain import tree
-import time
-import yaml
-import sys
 
 class Seawulf:
     def __init__(self, precincts_path, state,
@@ -17,20 +12,22 @@ class Seawulf:
                  assignment_criteria='districtId', random_initial_partition=True, epsilon=0.02, node_repeats=2,
                  cut_edges_multiplier=2, population_deviation=0.1, output_path=''):
         
-        self.precincts = geopandas.read_file(precincts_path)
+        self.precincts = final_precincts_co # geopandas.read_file(precincts_path)  
         self.district_list = list(self.precincts['districtId'].unique())
         self.state = state
         self.districts = None
-        self.graph = Graph.from_geodataframe(self.precincts)
+        self.graph = Graph.from_geodataframe(self.precincts, ignore_errors=True)
         self.random_initial_partition = random_initial_partition
         self.target_metric = target_metric
         self.assignment_criteria = assignment_criteria
         self.seawulf_updaters = self.create_updaters()
+        
+        print("the updaters: ", self.seawulf_updaters)
         self.epsilon = epsilon
         self.ideal_metric = self.precincts[self.target_metric].sum() / len(self.district_list)
         self.initial_partition = self.create_partition()
         self.proposal = self.create_proposal(node_repeats)
-        self.constraints = self.create_constraints(cut_edges_multiplier, population_deviation)
+        self.constraints = self.create_constraints(population_deviation)
         self.seawulf_districtPlans = pd.DataFrame(columns=['districtPlanId', 'state'])
         self.seawulf_districts = pd.DataFrame(
             columns=['districtId', 'districtPlanId', 'total_pop', 'white', 'af_amer', 'asian', 'hispanic', 'native_hawaiian', 'two_or_more',
@@ -51,19 +48,25 @@ class Seawulf:
     def create_updaters(self) -> dict:
         seawulf_updaters = {}
         for column in self.precincts.columns:
-            if column != 'geometry' and 'id' not in column.lower(): # don't put in the geometries or the ID's of anything in  the updaters
+            #print("column: ", column)
+            if column != 'geometry' and 'precinctId' not in column.lower(): # don't put in the geometries or the ID's of anything in  the updaters
                 seawulf_updaters[column] = updaters.Tally(column)
         return seawulf_updaters
 
     def create_proposal(self, node_repeats):
         return partial(recom, pop_col=self.target_metric, pop_target=self.ideal_metric, epsilon=self.epsilon,
                        node_repeats=node_repeats)
+    
+    def create_constraints(self, population_deviation) -> list:
+        population_constraint = constraints.within_percent_of_ideal_population(self.initial_partition,
+                                                                               population_deviation, pop_key="total_pop")
+        return [population_constraint]
 
     def process_iteration(self, partition, id) -> (pd.DataFrame, pd.DataFrame):
         precincts = []
     
         for node in partition.graph.nodes:
-            precinctId = partition.graph.nodes[node]['precinctID']
+            precinctId = partition.graph.nodes[node]['precinctId']
             districtId = "".join([id, str(partition.assignment[node])])
             total_pop = partition.graph.nodes[node]['total_pop']
             white = partition.graph.nodes[node]['white']
@@ -77,32 +80,32 @@ class Seawulf:
             republicanPres = partition.graph.nodes[node]['republicanPres']
             democraticSen = partition.graph.nodes[node]['democraticSen']
             republicanSen = partition.graph.nodes[node]['republicanSen']
-            
-            geometry = partition.graph.nodes[node]['geometry']
+            geometry = partition.graph.nodes[node]['geometry'].buffer(0)
             
             precincts.append([precinctId, districtId, total_pop, white, af_amer, asian, native_hawaiian,
                               hispanic, two_or_more, democraticPres, republicanPres,
                               democraticSen, republicanSen, geometry])
         
         precincts = pd.DataFrame(precincts,
-                                 columns=['precinctId', 'districtId', 'total_pop', 'white', 'af_amer', 'asian', 'hispanic', 'native_hawaiian', 'two_or_more',
+                                 columns=['precinctId', 'districtId', 'total_pop', 'white', 'af_amer', 'asian', 
+                                          'hispanic', 'native_hawaiian', 'two_or_more',
                      'democraticPres', 'republicanPres', 'democraticSen', 'republicanSen', 'geometry'])
 
         districts_info = precincts[
             ['districtId', 'total_pop', 'white', 'af_amer', 'asian', 'hispanic', 'native_hawaiian', 'two_or_more',
                      'democraticPres', 'republicanPres', 'democraticSen', 'republicanSen']].groupby(
-            'districtId').sum().reset_index()
+            'districtId').sum().reset_index()  # summing the precincts's columns by districtId
         
         districts_info['districtPlanId'] = id
         districts_geo = precincts[['districtId', 'geometry']].groupby('districtId')['geometry'].apply(
-            unary_union).reset_index() 
+            unary_union).reset_index() # shapely unary union does a union over all the precinct geometries
         
         districts = districts_info.merge(districts_geo, on='districtId')
         districtPlan = pd.DataFrame([[id, self.state]], columns=['districtPlanId', 'state'])
         return districtPlan, districts
     
-    def run(self, num_districtings, iterations):
-        for i in range(num_districtings):
+    def run(self, num_districtPlans, iterations):
+        for i in range(num_districtPlans):
             chain = MarkovChain(
                 proposal=self.proposal,
                 constraints=self.constraints,
@@ -115,29 +118,28 @@ class Seawulf:
             
             id = ''.join([self.state, 'SW', str(i)])
 
-            new_districting, new_districts = self.process_iteration(new_partition, id)
-            self.seawulf_districtings = self.seawulf_districtings.append(new_districting, ignore_index=True)
+            new_districtPlan, new_districts = self.process_iteration(new_partition, id)
+            self.seawulf_districtPlans = self.seawulf_districtPlans.append(new_districtPlan, ignore_index=True)
             self.seawulf_districts = self.seawulf_districts.append(new_districts, ignore_index=True)
             
-        self.seawulf_districtings.to_csv("".join([self.output_path, id, '_districtPlans.csv']), index=False)
+        self.seawulf_districtPlans.to_csv("".join([self.output_path, id, '_districtPlans.csv']), index=False)
         self.seawulf_districts.to_csv("".join([self.output_path, id, '_districts.csv']), index=False)
 
 if __name__ == '__main__':
     precincts_path = "/Users/cherrypi/cse416/processed_preseawulf_precincts_path/final_precinct_co.json"
     state = "Co"
-    num_districtings = 5
+    num_districtPlans = 5
     iterations = 5
-    assignment_criteria = 'districtID'
+    assignment_criteria = 'districtId'
     random_initial_partition = True
     epsilon = 0.02
     node_repeats = 2
     cut_edges_multiplier = 2
     population_deviation = 0.1
-    output_path = '/Users/cherrypi/cse416/postseawulf_graph_path'
+    output_path = '/Users/cherrypi/Desktop/gerrymandering-project/data/post-processing/postseawulf_graph_path/'
     
     seawulf = Seawulf(precincts_path=precincts_path, state=state, assignment_criteria=assignment_criteria,
                       random_initial_partition=random_initial_partition, epsilon=epsilon, node_repeats=node_repeats,
                       cut_edges_multiplier=cut_edges_multiplier, population_deviation=population_deviation,
                       output_path=output_path)
-    seawulf.run(num_districtings=num_districtings, iterations=iterations)
-
+    seawulf.run(num_districtPlans=num_districtPlans, iterations=iterations)
