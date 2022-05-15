13f699076216a861f7920fe88f50c83557ca8c19
Lolita Nazarov <ln3141592653@gmail.com>
2022-05-15 17:07:35 -0400

    histogram seawulf ensemble data generated, box&whisker works, all measures computed for everything

--- data/post-processing/create_box_and_whisker_plot_data.py
+++ data/post-processing/create_box_and_whisker_plot_data.py
@@ -35,73 +35,95 @@ def getBoxAndWhiskerPoints(districtNestedList): # array of all districts for 1 c
         newDistrictNestedList.append(stats)
     return newDistrictNestedList
 
-    processed_path = '/Users/cherrypi/Desktop/gerrymandering-project/data/post-processing/processed_districtplan_path/co_processed_DPs_percents'
+processed_path_co = '/Users/cherrypi/Desktop/gerrymandering-project/data/post-processing/processed_districtplan_path_seawulf/co_processed_plans/'
 
+processed_path_tn = '/Users/cherrypi/Desktop/gerrymandering-project/data/post-processing/processed_districtplan_path_seawulf/tn_processed_plans/'
+
+processed_path_sc = '/Users/cherrypi/Desktop/gerrymandering-project/data/post-processing/processed_districtplan_path_seawulf/sc_processed_plans/'
+
+def generate_box_and_whisker_data_for_state(processedStateDistrictPlansFilePath):
     # count how many districts are in 1 district Plan, that's the number of districts for all district Plans FOR THIS STATE
-numDistricts=0
-for item in os.listdir(processed_path):
-    if (item.endswith('.json')):
-        #print(item)
-        plan_file = open(os.path.join(processed_path, item), "r")
-        plan_data = json.load(plan_file)
-        print(plan_data['Percents'].keys())
-        numDistricts=len(plan_data['Percents']['white'])
-        break
-
-# [10,000 district plan points for district 1], [10,000 district plan points for district 2], [10,000 points for district 3], etc
-white_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
-af_amer_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
-asian_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
-hispanic_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
-native_hawaiian_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
-two_or_more_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
-democraticPres_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
-republicanPres_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
-democraticSen_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
-republicanSen_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
-
-for item in os.listdir(processed_path): # iterating through every single district plan
-    if (item.endswith('.json')):
-        #print(item)
-        plan_file = open(os.path.join(processed_path, item), "r")
-        plan_data = json.load(plan_file)
+    numDistricts=0
+    stateId=0  # for the box and whisker path file
+    
+    for item in os.listdir(processedStateDistrictPlansFilePath):
+        if (item.endswith('.json')):
+            #print(item)
+            plan_file = open(os.path.join(processedStateDistrictPlansFilePath, item), "r")
+            plan_data = json.load(plan_file)
+            #print(plan_data['Box_Whisker_Percents'].keys())
+            numDistricts=len(plan_data['Box_Whisker_Percents']['white'])
+            stateId=plan_data['stateId']
+            
+            break
+    
+    if stateId=='5':
+        stateId="Co"
+    elif stateId=='39':
+        stateId="Sc"
+    elif stateId=='41':
+        stateId="Tn"
         
-        for i in range(numDistricts): # iterating through every number in each district
-            white_districts[i].append(float(plan_data["Percents"]['white'][i]))
-            af_amer_districts[i].append(float(plan_data["Percents"]['af_amer'][i]))
-            asian_districts[i].append(float(plan_data["Percents"]['asian'][i]))
-            hispanic_districts[i].append(float(plan_data["Percents"]['hispanic'][i]))
-            native_hawaiian_districts[i].append(float(plan_data["Percents"]['native_hawaiian'][i]))
-            two_or_more_districts[i].append(float(plan_data["Percents"]['two_or_more'][i]))
-            democraticPres_districts[i].append(float(plan_data["Percents"]['democraticPres'][i]))
-            republicanPres_districts[i].append(float(plan_data["Percents"]['republicanPres'][i]))
-            democraticSen_districts[i].append(float(plan_data["Percents"]['democraticSen'][i]))
-            republicanSen_districts[i].append(float(plan_data["Percents"]['republicanSen'][i]))
+    # [10,000 district plan points for district 1], [10,000 district plan points for district 2], 
+    # [10,000 points for district 3], etc
+    white_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
+    af_amer_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
+    asian_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
+    hispanic_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
+    native_hawaiian_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
+    two_or_more_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
+    democraticPres_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
+    republicanPres_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
+    democraticSen_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
+    republicanSen_districts= [[] for _ in range(numDistricts)] # nested arr, by district 
+    
+    for item in os.listdir(processedStateDistrictPlansFilePath): # iterating through every single district plan
+        if (item.endswith('.json')):
+            #print(item)
+            plan_file = open(os.path.join(processedStateDistrictPlansFilePath, item), "r")
+            plan_data = json.load(plan_file)
 
-white_districts_bw=getBoxAndWhiskerPoints(white_districts)
-af_amer_districts_bw=getBoxAndWhiskerPoints(af_amer_districts)
-asian_districts_bw=getBoxAndWhiskerPoints(asian_districts)
-hispanic_districts_bw=getBoxAndWhiskerPoints(hispanic_districts)
-native_hawaiian_districts_bw=getBoxAndWhiskerPoints(native_hawaiian_districts)
-two_or_more_districts_bw=getBoxAndWhiskerPoints(two_or_more_districts)
-democraticPres_districts_bw=getBoxAndWhiskerPoints(democraticPres_districts)
-republicanPres_districts_bw=getBoxAndWhiskerPoints(republicanPres_districts)
-democraticSen_districts_bw=getBoxAndWhiskerPoints(democraticSen_districts)
-republicanSen_districts_bw=getBoxAndWhiskerPoints(republicanSen_districts)
+            for i in range(numDistricts): # iterating through every number in each district
+                white_districts[i].append(float(plan_data["Box_Whisker_Percents"]['white'][i]))
+                af_amer_districts[i].append(float(plan_data["Box_Whisker_Percents"]['af_amer'][i]))
+                asian_districts[i].append(float(plan_data["Box_Whisker_Percents"]['asian'][i]))
+                hispanic_districts[i].append(float(plan_data["Box_Whisker_Percents"]['hispanic'][i]))
+                native_hawaiian_districts[i].append(float(plan_data["Box_Whisker_Percents"]['native_hawaiian'][i]))
+                two_or_more_districts[i].append(float(plan_data["Box_Whisker_Percents"]['two_or_more'][i]))
+                democraticPres_districts[i].append(float(plan_data["Box_Whisker_Percents"]['democraticPres'][i]))
+                republicanPres_districts[i].append(float(plan_data["Box_Whisker_Percents"]['republicanPres'][i]))
+                democraticSen_districts[i].append(float(plan_data["Box_Whisker_Percents"]['democraticSen'][i]))
+                republicanSen_districts[i].append(float(plan_data["Box_Whisker_Percents"]['republicanSen'][i]))
 
-boxWhisk = {}
-boxWhisk['white'] = white_districts_bw
-boxWhisk['af_amer'] = af_amer_districts_bw
-boxWhisk['asian'] = asian_districts_bw
-boxWhisk['hispanic'] = hispanic_districts_bw
-boxWhisk['native_hawaiian'] = native_hawaiian_districts_bw
-boxWhisk['two_or_more'] = two_or_more_districts_bw
-boxWhisk['democraticPres'] = democraticPres_districts_bw
-boxWhisk['republicanPres'] = republicanPres_districts_bw
-boxWhisk['democraticSen'] = democraticSen_districts_bw
-boxWhisk['republicanSen'] = republicanSen_districts_bw
+    white_districts_bw=getBoxAndWhiskerPoints(white_districts)  # returns an array of 5 values (min, q1, med, q3, max)
+    af_amer_districts_bw=getBoxAndWhiskerPoints(af_amer_districts)
+    asian_districts_bw=getBoxAndWhiskerPoints(asian_districts)
+    hispanic_districts_bw=getBoxAndWhiskerPoints(hispanic_districts)
+    native_hawaiian_districts_bw=getBoxAndWhiskerPoints(native_hawaiian_districts)
+    two_or_more_districts_bw=getBoxAndWhiskerPoints(two_or_more_districts)
+    democraticPres_districts_bw=getBoxAndWhiskerPoints(democraticPres_districts)
+    republicanPres_districts_bw=getBoxAndWhiskerPoints(republicanPres_districts)
+    democraticSen_districts_bw=getBoxAndWhiskerPoints(democraticSen_districts)
+    republicanSen_districts_bw=getBoxAndWhiskerPoints(republicanSen_districts)
+    
+    boxWhisk = {}
+    boxWhisk['white'] = white_districts_bw
+    boxWhisk['af_amer'] = af_amer_districts_bw
+    boxWhisk['asian'] = asian_districts_bw
+    boxWhisk['hispanic'] = hispanic_districts_bw
+    boxWhisk['native_hawaiian'] = native_hawaiian_districts_bw
+    boxWhisk['two_or_more'] = two_or_more_districts_bw
+    boxWhisk['democraticPres'] = democraticPres_districts_bw
+    boxWhisk['republicanPres'] = republicanPres_districts_bw
+    boxWhisk['democraticSen'] = democraticSen_districts_bw
+    boxWhisk['republicanSen'] = republicanSen_districts_bw
 
-file_name = "/Users/cherrypi/Desktop/gerrymandering-project/data/post-processing/box_whisk.json"
-file1 = open(file_name, "w")
-json.dump(boxWhisk, file1)
+    file_name = '/Users/cherrypi/Desktop/gerrymandering-project/data/post-processing/box_and_whisker_seawulf/box_whisk_' + str(stateId) + '.json' 
+    file1 = open(file_name, "w")
+    json.dump(boxWhisk, file1)
 
+if __name__ == "__main__":
+    
+    generate_box_and_whisker_data_for_state(processed_path_co)
+    generate_box_and_whisker_data_for_state(processed_path_tn)
+    generate_box_and_whisker_data_for_state(processed_path_sc)
--- data/seawulf_mggg.py
+++ data/seawulf_mggg.py
@@ -1,7 +1,6 @@
 from shapely.ops import unary_union
 from shapely import wkt
 import pandas as pd
-import geopandas as gpd
 
 from gerrychain.accept import always_accept
 import numpy as np
@@ -34,7 +33,7 @@ class Seawulf:
         self.constraints = self.create_constraints(population_deviation)
         self.seawulf_districtPlans = pd.DataFrame(columns=['districtPlanId', 'state'])
         self.seawulf_districts = pd.DataFrame(
-            columns=['districtId', 'districtPlanId', 'total_pop', 'white', 'af_amer', 'asian', 'hispanic', 'native_hawaiian', 'two_or_more',
+            columns=['districtId', 'districtPlanId', 'stateId', 'total_pop', 'white', 'af_amer', 'asian', 'hispanic', 'native_hawaiian', 'two_or_more',
                      'democraticPres', 'republicanPres', 'democraticSen', 'republicanSen', 
                      'geometry'])
         self.output_path = output_path
@@ -72,6 +71,7 @@ class Seawulf:
         for node in partition.graph.nodes:
             precinctId = partition.graph.nodes[node]['precinctId']
             districtId = "".join([id, str(partition.assignment[node])])
+            stateId = self.state # partition.graph.nodes[node]['stateId']
             total_pop = partition.graph.nodes[node]['total_pop']
             white = partition.graph.nodes[node]['white']
             af_amer = partition.graph.nodes[node]['af_amer']
@@ -86,17 +86,17 @@ class Seawulf:
             republicanSen = partition.graph.nodes[node]['republicanSen']
             geometry = partition.graph.nodes[node]['geometry'].buffer(0)
             
-            precincts.append([precinctId, districtId, total_pop, white, af_amer, asian, native_hawaiian,
+            precincts.append([precinctId, districtId, stateId, total_pop, white, af_amer, asian, native_hawaiian,
                               hispanic, two_or_more, democraticPres, republicanPres,
                               democraticSen, republicanSen, geometry])
         
         precincts = pd.DataFrame(precincts,
-                                 columns=['precinctId', 'districtId', 'total_pop', 'white', 'af_amer', 'asian', 
+                                 columns=['precinctId', 'districtId', 'stateId', 'total_pop', 'white', 'af_amer', 'asian', 
                                           'hispanic', 'native_hawaiian', 'two_or_more',
                      'democraticPres', 'republicanPres', 'democraticSen', 'republicanSen', 'geometry'])
 
         districts_info = precincts[
-            ['districtId', 'total_pop', 'white', 'af_amer', 'asian', 'hispanic', 'native_hawaiian', 'two_or_more',
+            ['districtId', 'stateId', 'total_pop', 'white', 'af_amer', 'asian', 'hispanic', 'native_hawaiian', 'two_or_more',
                      'democraticPres', 'republicanPres', 'democraticSen', 'republicanSen']].groupby(
             'districtId').sum().reset_index()  # summing the precincts's columns by districtId
         
@@ -108,8 +108,7 @@ class Seawulf:
         districtPlan = pd.DataFrame([[id, self.state]], columns=['districtPlanId', 'state'])
         return districtPlan, districts
     
-    def run(self, num_districtPlans, iterations):
-        
+    def run(self, num_districtPlans, iterations): 
         for i in range(num_districtPlans):
             chain = MarkovChain(
                 proposal=self.proposal,
@@ -130,18 +129,22 @@ class Seawulf:
         self.seawulf_districtPlans.to_csv("".join([self.output_path, id, '_districtPlans.csv']), index=False)
         self.seawulf_districts.to_csv("".join([self.output_path, id, '_districts.csv']), index=False)
 
+
 if __name__ == '__main__':
     # load from csv to DataFrame for SeaWulf to run
-    df = pd.read_csv("/Users/cherrypi/Desktop/gerrymandering-project/data/pre-processing/preseawulf_precincts_path/final_precincts_preseawulf_co.csv")
+    df = pd.read_csv("/Users/cherrypi/Desktop/gerrymandering-project/data/pre-processing/preseawulf_precincts_path/final_precincts_preseawulf_tn.csv")
     df['geometry'] = df['geometry'].apply(wkt.loads)
-    precincts_gdf = gpd.GeoDataFrame(df, crs='epsg:4326')
+    precincts_gdf = gpd.GeoDataFrame(df, crs='epsg:4269')
     
-    if "Unnamed: 0" in precincts.columns:
+    if "Unnamed: 0" in precincts_gdf.columns:
         precincts_gdf.drop(["Unnamed: 0"], axis=1, inplace=True)
-
-    # insert code here
+    '''
+    5 -> CO
+    39 -> SC
+    41 -> TN
+    '''
     precincts_df = precincts_gdf
-    state = "Co"
+    state = "Tn"
     num_districtPlans = 10
     iterations = 10
     assignment_criteria = 'districtId'
@@ -150,7 +153,7 @@ if __name__ == '__main__':
     node_repeats = 2
     cut_edges_multiplier = 2
     population_deviation = 0.1
-    output_path = '/Users/cherrypi/Desktop/gerrymandering-project/data/post-processing/postseawulf_graph_path/'
+    output_path = '/Users/cherrypi/Desktop/gerrymandering-project/data/post-processing/postseawulf_CSV_path/'
     
     seawulf = Seawulf(precincts_df=precincts_gdf, state=state, assignment_criteria=assignment_criteria,
                       random_initial_partition=random_initial_partition, epsilon=epsilon, node_repeats=node_repeats,
